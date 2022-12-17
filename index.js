b39aaaa36bc1a4b8e89edc10fdc27e140a9d40a1
RobinKohrs <robin.kohrs@gmx.de>
2022-12-17 09:37:54 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,27 @@
+let express = require("express");
+let SunCalc = require("suncalc");
+let bodyParser = require("body-parser");
+
+const APP = express();
+APP.use(bodyParser.urlencoded({ extended: false }));
+APP.use(bodyParser.json());
+
+APP.listen(process.env.PORT || 3000, () => {
+  console.log(`server is running`);
+});
+
+APP.get("/sunpos", (req, res) => {
+  let lat = req.query.lat;
+  let lon = req.query.lon;
+  let date = req.query.date;
+
+  date = new Date(date);
+
+  let pos = SunCalc.getPosition(
+    /*Date*/ date,
+    /*Number*/ +lat,
+    /*Number*/ +lon
+  );
+
+  res.json(pos);
+});
